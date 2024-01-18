cf74d908966d27f89192211da8f7b36d67ecff47
Ariel <amvaldes010117@gmail.com>
2024-01-18 15:38:30 +0100

    update

--- app/estructura/read_data_csv.py
+++ app/estructura/read_data_csv.py
@@ -2,8 +2,7 @@ import csv
 from app.estructura.catalogo import Juegos
 
 
-path = "datos\\vgsales.csv"
-
+path = 'datos/vgsales.csv'
 def leer_datos():
     with open(path, 'r') as f:
         lectura = csv.reader(f)
@@ -12,5 +11,3 @@ def leer_datos():
             Juegos.inser_data(lista)
         x = Juegos.lista_juegos
         print(x)
-
-leer_datos()
---
+++ main.py
@@ -0,0 +1,9 @@
+from app.estructura.read_data_csv import leer_datos
+
+
+def main():
+    leer_datos()
+    
+
+if __name__ == '__main__':
+    main()
