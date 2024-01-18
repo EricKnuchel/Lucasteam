2c50030f5138d3f0adbee1885f013123c348de83
Ariel <amvaldes010117@gmail.com>
2024-01-18 13:18:21 +0100

    first commit

---
+++ app/estructura/catalogo.py
@@ -0,0 +1,22 @@
+class Juegos:
+    lista_juegos = []
+
+    @classmethod
+    def inser_data(cls, data):
+        j = Juego(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10])
+        cls.lista_juegos.append(j)
+
+
+class Juego:
+    def __init__(self, rank, name, plataf, year, genero, editor, v_na, v_eu, v_jp, v_otras, v_glob):
+        self.rank = rank
+        self.name = name
+        self.plataf = plataf
+        self.year = year
+        self.genero = genero
+        self.editor = editor
+        self.v_na = v_na
+        self.v_eu = v_eu
+        self.v_jp = v_jp
+        self.v_otras = v_otras
+        self.v_glob = v_glob
---
+++ app/estructura/read_data_csv.py
@@ -0,0 +1,16 @@
+import csv
+from app.estructura.catalogo import Juegos
+
+
+path = "datos\\vgsales.csv"
+
+def leer_datos():
+    with open(path, 'r') as f:
+        lectura = csv.reader(f)
+        for l in lectura:
+            lista = l
+            Juegos.inser_data(lista)
+        x = Juegos.lista_juegos
+        print(x)
+
+leer_datos()
