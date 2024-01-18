9fc3ca74b454e3be234e80b390d0968cf084c029
Ariel <amvaldes010117@gmail.com>
2024-01-18 19:24:12 +0100

    Subiendo el proyecto a la rama ari

--- app/estructura/catalogo.py
+++ app/estructura/catalogo.py
@@ -4,7 +4,7 @@ class Juegos:
     @classmethod
     def inser_data(cls, data):
         j = Juego(data[0], data[1], data[2], data[3], data[4], data[5], data[6], data[7], data[8], data[9], data[10])
-        cls.lista_juegos.append(j)
+        return cls.lista_juegos.append(j)
 
 
 class Juego:
@@ -20,3 +20,11 @@ class Juego:
         self.v_jp = v_jp
         self.v_otras = v_otras
         self.v_glob = v_glob
+    
+    # Este es un metodo de Python que se usa para definir el comportamiento de el operador de igualdad (==)
+    def __eq__(self, other):
+        """# verificamos si other es una instancia de la clase Juegos
+        si no lo es no se considerarian iguales y retorna false"""
+        if not isinstance(other, Juego):
+            return False
+        return vars(self) == vars(other)
