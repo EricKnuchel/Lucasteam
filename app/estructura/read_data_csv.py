741494fdca3878175a968ebbe00a7939c6d66369
Ariel <amvaldes010117@gmail.com>
2024-01-19 12:56:57 +0100

    interfaz grafica terminada , y funsionabilidades activas,a esperada de la implementacion de las validadciones

--- app/estructura/read_data_csv.py
+++ app/estructura/read_data_csv.py
@@ -1,19 +1,17 @@
 import csv
 from app.estructura.catalogo import Juegos
 
+
 def leer_datos():
     try:
         with open('datos/vgsales.csv', 'r') as f:
             lectura = csv.reader(f)
             next(lectura)
             for i, l in enumerate(lectura):
-                if i >= 1000:
+                if i >= 5:
                     break
                 lista = l
                 Juegos.inser_data(lista)
-            # x = Juegos.lista_juegos
-            # print(x[0].name)
-            print("Si se imprime este mensaje funciona correcto.")
 
     except FileNotFoundError:
         print("Archivo no encontrado")
@@ -23,4 +21,3 @@ def leer_datos():
         print("Error al leer los datos del CSV")
     except Exception as e:
         print(f"A orcurrido un error: {e}")
-
--- gui/interfaz.py
+++ gui/interfaz.py
@@ -1,49 +1,5 @@
-from tkinter import ttk, Tk,Button, Toplevel
+from tkinter import ttk, Tk, Button, Toplevel, Label, Entry, StringVar
 from app.estructura.catalogo import Juegos
-from app.estructura.read_data_csv import leer_datos
-# # Datos de prueba 
-# data = [
-#     (1, 'Game1', 'Platform1', 2000, 'Genre1', 'Publisher1', 1.0, 0.5, 0.2, 0.1, 1.8),
-#     (2, 'Game2', 'Platform2', 2005, 'Genre2', 'Publisher2', 0.8, 0.3, 0.5, 0.2, 1.8),
-# ]
-
-# # Función para configurar el Treeview con los datos
-# def encabezado(tree):
-#     # esta funsion recibe como parametro una treeview que es ventana con las columnas que definimos ene ste codigo
-#     columns = ("Rank", "Name", "Platform", "Year", "Genre", "Publisher",
-#                "NA_Sales", "EU_Sales", "JP_Sales", "Other_Sales", "Global_Sales")
-
-#     tree["columns"] = columns
-#     tree["show"] = "headings"
-
-#     for col in columns:
-#         tree.heading(col, text=col)
-#         tree.column(col, anchor="center", width=75)  # aqui se ajusta el año de la tabla
-        
-#     for row in data:
-#         tree.insert("", "end", values=row)
-
-# def mostrar_all_games():
-#     # Configuración de la ventana principal
-#     root = Tk()
-#     root.title("Mostrar lista")
-#     root.resizable(0, 0)
-
-#     # Creación del Treeview
-#     tree = ttk.Treeview(root)
-#     encabezado(tree)
-
-#     # Configuración del scrollbar vertical
-#     scrollbar = ttk.Scrollbar(root, orient="vertical", command=tree.yview)
-#     scrollbar.pack(side="right", fill="y")
-
-#     tree.configure(yscrollcommand=scrollbar.set)
-
-#     # Empaqueta el Treeview en la ventana
-#     tree.pack(expand=True, fill="both")
-
-#     # Inicia el bucle principal de la aplicación
-#     return root.mainloop()
 
 
 class Ventana:
@@ -52,32 +8,114 @@ class Ventana:
         self.master.title("Video Game Sales")
         self.master.resizable(0, 0)
         self.master.configure(bg='#FF9EA0')
-        self.master.geometry("300x400")
-
-        # Datos de ejemplo
-        self.data = [
-            (1, 'Game1', 'Platform1', 2000, 'Genre1', 'Publisher1', 1.0, 0.5, 0.2, 0.1, 1.8),
-            (2, 'Game2', 'Platform2', 2005, 'Genre2', 'Publisher2', 0.8, 0.3, 0.5, 0.2, 1.8),
-        ]
+        self.master.geometry("300x250")
+        self.master.eval(f'tk::PlaceWindow {str(self.master)} center')
 
         # Configuración de la ventana principal
         self.setup_main_window()
 
     def setup_main_window(self):
         # Botones en la ventana principal
-        insert_button = Button(self.master, text="Insertar Datos", command=self.insert_data)
+        insert_button = Button(self.master, text="Datos Manual", command=self.insert_data)
         insert_button.pack(pady=10)
 
         show_button = Button(self.master, text="Mostrar Lista de Juegos", command=self.show_list)
         show_button.pack(pady=10)
-        
-        show_xx = Button(self.master, text="Mostrar Juegos Siglo XX", command=Juegos.show_games_siglo_xx)
-        show_xx.pack(pady=10)
+
+        # show_xx = Button(self.master, text="Mostrar Juegos Siglo XX", command=Juegos.show_games_siglo_xx)
+        # show_xx.pack(pady=10)
 
     def insert_data(self):
-        # lógica para insertar datos
-        self.data.append((len(self.data) + 1, f'Game{len(self.data) + 1}', 'Platform', 2200, 'Genre', 'Publisher', 1.0, 0.5, 0.2, 0.1, 1.8))
-        print("Datos insertados")
+        window = Tk()
+        window.title("Incert Juegos")
+        window.resizable(0, 0)
+        window.configure(bg='#FF9EA0')
+        window.geometry("250x350")
+
+        manual_name_label = Label(window, text="Nombre", bg='#FF9EA0')
+        manual_name_label.pack()
+        manual_name = Entry(window)
+        manual_name.pack()
+        manual_name.insert(0, "Ej: Mario Bros")
+        manual_name.config(fg="grey")
+        manual_name.bind("<FocusIn>",
+                         lambda event: manual_name.delete(0, 'end') if manual_name.get() == "Ej: Mario Bros" else None)
+        manual_name.bind("<FocusOut>",
+                         lambda event: manual_name.insert(0, "Ej: Mario Bros") if manual_name.get() == "" else None)
+
+        manual_plat_l = Label(window, text="Plataforma", bg='#FF9EA0')
+        manual_plat_l.pack()
+        var_plataforma = StringVar(window)
+        var_plataforma.set("Ninguna")
+        plataformas = ["Ninguna", "Wii", "NES", "GB", "X360", "PS", "PS2", "PS3", "PS4", "PS5", "SNES", "GBA", "3DS",
+                       "N64", "XB", "2600", "DS", "XOne", "GC", "GEN", "PSP", "WiiU", "PC", "DC", ]
+        plataforma_combobox = ttk.Combobox(window, textvariable=var_plataforma, values=plataformas, state="readonly")
+        plataforma_combobox.pack()
+
+        manual_year_l = Label(window, text="Año", bg='#FF9EA0')
+        manual_year_l.pack()
+        manual_years = StringVar(window)
+        manual_years.set("1952")
+        manual_vears_list = [str(manual_vears_list) for manual_vears_list in range(1952, 2024)]
+        manual_plataform_year_menu = ttk.Combobox(window, textvariable=manual_years, values=manual_vears_list,
+                                                  state="readonly")
+        manual_plataform_year_menu.pack()
+
+        manual_genero_l = Label(window, text="Genero", bg='#FF9EA0')
+        manual_genero_l.pack()
+        manual_genero = StringVar(window)
+        manual_genero.set("Ninguna")
+        manual_genero_lista = ["Sports", "Racing", "Role-Playing", "Puzzle", "Platform", "Misc", "Shooter",
+                               "Simulation", "Action", "Fighting", "Adventure", "Strategy"]
+        genero_combobox = ttk.Combobox(window, textvariable=manual_genero, values=manual_genero_lista, state="readonly")
+        genero_combobox.pack()
+
+        manual_editor_l = Label(window, text="Editor", bg='#FF9EA0')
+        manual_editor_l.pack()
+        manual_editor = StringVar(window)
+        manual_editor.set("Ninguna")
+        manual_editor_lista = ["Desconocido", "Nintendo", "Microsoft Game Studios", "Take-Two Interactive",
+                               "Sony Computer Entertainment", "Activision", "Ubisoft", "Bethesda Softworks",
+                               "Electronic Arts", "Sega", "Square Enix", "Atari", "505 Games", "Capcom",
+                               "GT Interactive", "Konami Digital Entertainment", "Namco Bandai Games",
+                               "Warner Bros. Interactive Entertainment", "Majesco Entertainment", "Codemasters",
+                               "RedOctane", "THQ", "Fox Interactive", "Universal Interactive", "LucasArts",
+                               "Virgin Interactive", "Palcom", "Hasbro Interactive", "Vivendi Games", "NCSoft",
+                               "Deep Silver", "Arena Entertainment", "Valve Software", "ASCII Entertainment",
+                               "Mindscape", "Infogrames"]
+        editor_combobox = ttk.Combobox(window, textvariable=manual_editor, values=manual_editor_lista, state="readonly")
+        editor_combobox.pack()
+
+        manual_ventas_na_l = Label(window, text="Vents NA", bg='#FF9EA0')
+        manual_ventas_na_l.place(x=5, y=220)
+        manual_ventas_na_e = Entry(window, width=4)
+        manual_ventas_na_e.place(x=15, y=240)
+
+        manual_ventas_eu_l = Label(window, text="Vents EU", bg='#FF9EA0')
+        manual_ventas_eu_l.place(x=70, y=220)
+        manual_ventas_eu_e = Entry(window, width=4)
+        manual_ventas_eu_e.place(x=80, y=240)
+
+        manual_ventas_jp_l = Label(window, text="Vents JP", bg='#FF9EA0')
+        manual_ventas_jp_l.place(x=130, y=220)
+        manual_ventas_jp_e = Entry(window, width=4)
+        manual_ventas_jp_e.place(x=140, y=240)
+
+        manual_ventas_ov_l = Label(window, text="Other Vents", bg='#FF9EA0')
+        manual_ventas_ov_l.place(x=180, y=220)
+        manual_ventas_ov_e = Entry(window, width=4)
+        manual_ventas_ov_e.place(x=200, y=240)
+
+        manual_ventas_vg_l = Label(window, text="Vents Globals", bg='#FF9EA0')
+        manual_ventas_vg_l.place(x=85, y=270)
+        manual_ventas_vg_e = Entry(window, width=4)
+        manual_ventas_vg_e.place(x=110, y=290)
+
+        manual_button = Button(window, text="Insert", command=lambda: Juegos.inser_data(
+            [len(Juegos.lista_juegos) + 1, manual_name.get(), var_plataforma.get(), manual_years.get(),
+             manual_genero.get(), manual_editor.get(), manual_ventas_na_e.get(), manual_ventas_eu_e.get(),
+             manual_ventas_jp_e.get(), manual_ventas_ov_e.get(), manual_ventas_vg_e.get()]))
+        manual_button.place(x=105, y=320)
 
     def show_list(self):
         # Crea una ventana secundaria para mostrar la lista de juegos
@@ -89,7 +127,6 @@ class Ventana:
         # Creación del Treeview en la ventana secundaria
         tree = ttk.Treeview(window)
         self.setup_treeview(tree)
-        
 
         # Inserta los datos en el Treeview
         for juego in Juegos.lista_juegos:
@@ -116,7 +153,7 @@ class Ventana:
 
         for col in columns:
             tree.heading(col, text=col)
-            tree.column(col, anchor="center", width=60) # aqui se ajusta el año de la tabla
+            tree.column(col, anchor="center", width=60)  # aqui se ajusta el año de la tabla
 
 
 def run_gui():
