3b8b0f9bd886e9db41c26b4ee32b21f8127e5ff3
Eric <eric.francisco.knuchel@gmail.com>
2024-01-23 12:10:14 +0100

    fixed leer_datos()

--- app/estructura/load_data_csv.py
+++ app/estructura/load_data_csv.py
@@ -6,33 +6,29 @@ from app.db.conexion_db import conectar_a_mysql
 
 def leer_datos():
     conn = conectar_a_mysql()
+    
+    if conn:
+        cursor = conn.cursor()
+        
     try:
+        sql = "SELECT COUNT(id) FROM Juegos"
+        cursor.execute(sql)
+        num_dat = cursor.fetchone()
+        
         with open('datos/vgsales.csv', 'r') as f:
             lectura = csv.reader(f)
             next(lectura)
             for l in lectura:
                 lista = l
-                # Juegos.inser_data(lista)
-                """if conn:
-                    try:
-                        cursor = conn.cursor()
-                        #sql = "SELECT id FROM Juegos"
-                        #cursor.execute(sql)
-                        #lista_id = cursor.fetchall()
-                        #j = Juego(lista[0], lista[1], lista[2], lista[3], lista[4], lista[5], lista[6], lista[7], lista[8], lista[9], lista[10])
-                        
-                        #if Juegos.alta_juego(j):
-                        sql = "INSERT INTO Juegos (nombre, plataforma, year, genero, publisher, V_NA, V_EU, V_JP, V_other, V_Global) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-                        val = (lista[1], lista[2], lista[3], lista[4], lista[5], lista[6], lista[7], lista[8], lista[9], lista[10])
-                        
-                        cursor.execute(sql,val)
-                        
-                        conn.commit()
-                        
-                        cursor.close()
-                    except mysql.connector.Error as e:
-                        print(f"Error de conexión: {e}")"""
                 Juegos.inser_data(lista)
+                
+                if num_dat[0] == 0:
+                    sql = "INSERT INTO Juegos (nombre, plataforma, year, genero, publisher, V_NA, V_EU, V_JP, V_other, V_Global) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+                    val = (lista[1], lista[2], lista[3], lista[4], lista[5], lista[6], lista[7], lista[8], lista[9], lista[10])
+                    
+                    cursor.execute(sql,val)
+                    
+                    conn.commit()
 
     except FileNotFoundError:
         print("Archivo no encontrado")
@@ -43,8 +39,8 @@ def leer_datos():
     except Exception as e:
         print(f"Ha ocurrido un error: {e}")
     finally:
+        cursor.close()
         conn.close()
-        print("Conexion a DB cerrada")
 
 
 def insert_data_db():
