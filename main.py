e00b5b844ef99af4740b60900026975ee3c9488d
Ariel <amvaldes010117@gmail.com>
2024-01-26 08:13:44 +0100

    correcion de colores en la interfaz principal

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
+import logging
 from app.db.conexion_db import create_table
 from app.estructura.load_data_csv import load_db, load_list
 from gui.interfaz import run_gui
-import logging
 
-#Creación del archivo donde se alojaran los logs del proyecto
+#Creación del archivo donde se alojaran los logs o registros del proyecto
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s [%(levelname)s] %(message)s', filename='log/proyecto_juegos_log.log', filemode = "w")
 
 def main():
-    create_table()
-    load_db()
-    load_list()
+    # create_table()
+    # load_db()
+    # load_list()
     run_gui()
 
 
